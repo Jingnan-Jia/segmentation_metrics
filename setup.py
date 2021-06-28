0fd41ff91c647a48401560d60b89cf8a5476ca98
Jingnan Jia <34941987+Jingnan-Jia@users.noreply.github.com>
2021-06-28 16:31:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="seg_metrics", # Replace with your own username
-    version="1.0.4",
+    version="1.0.7",
     author="Jingnan",
     author_email="jiajingnan2222@gmail.com",
     description="A package to compute different segmentation metrics for Medical images.",
