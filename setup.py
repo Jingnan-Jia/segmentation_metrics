792ad1b3aa5b13f5e8a75780b55c71a738814c64
jjia <972931271@qq.com>
2022-05-19 21:50:36 +0200

    v1.0.26 setup.py automatically install dependencies.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ def parse_requirements(requirements):
     with open(requirements) as f:
         return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
 
-reqs = parse_requirements('requirements.txt')
+# reqs = parse_requirements('requirements.txt')
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="seg_metrics", # Replace with your own username
-    version="1.0.25",
+    version="1.0.26",
     author="Jingnan",
     author_email="jiajingnan2222@gmail.com",
     description="A package to compute different segmentation metrics for Medical images.",
@@ -25,5 +25,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=reqs,
-)
+    install_requires=['pandas', 'numpy', 'coverage', 'matplotlib', 'parameterized', 'tqdm', 'medutils', 'PySimpleGUI', 'SimpleITK'])
