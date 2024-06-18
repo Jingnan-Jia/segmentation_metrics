fix a bug for the calculation of hd95 when label does not exist in ground truth and prediction

seg_metrics/seg_metrics.py
@@ -171,7 +171,10 @@ def computeQualityMeasures(lP: np.ndarray,
         quality["msd"] = np.mean(all_surface_distances)
         quality["mdsd"] = np.median(all_surface_distances)
         quality["stdsd"] = np.std(all_surface_distances)
-        quality["hd95"] = np.percentile(all_surface_distances, 95)
+        if len(all_surface_distances) == 0:  
+            quality["hd95"] = 0
+        else:
+            quality["hd95"] = np.percentile(all_surface_distances, 95)
         quality["hd"] = np.max(all_surface_distances)
     return quality
 